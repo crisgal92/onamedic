5112c588b093d9a878f21eadf5d19a4ed34e5785
crisgal92 <cristian_agm92@hotmail.com>
2024-11-09 13:45:06 -0600

    se agerga hiperviculo y se modifica el formulario

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { ContactoComponent } from './contacto/contacto.component';
 import { IndexComponent } from './index/index.component';
 
+//En esta parte se muestra las rutas para el url y el componente al que le llama 
 const routes: Routes = [
- { path: '', component: IndexComponent },
+ { path: '', component: IndexComponent }, // Te sirve para cuando el enlace esta en blanco nos manda directo al comoponente index
   {path: 'contacto', component: ContactoComponent},
   {path: 'index', component:  IndexComponent },
   { path: '**', redirectTo: '', pathMatch: 'full' }
