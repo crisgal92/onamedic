4197f386cc45f83d53779d208b9605462c344616
crisgal92 <cristian_agm92@hotmail.com>
2024-11-11 19:27:25 -0600

    se agrega el hipervinculo para equipos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContactoComponent } from './contacto/contacto.component';
 import { IndexComponent } from './index/index.component';
+import { EquiposComponent } from './equipos/equipos.component';
 
 //En esta parte se muestra las rutas para el url y el componente al que le llama 
 const routes: Routes = [
  { path: '', component: IndexComponent }, // Te sirve para cuando el enlace esta en blanco nos manda directo al comoponente index
   {path: 'contacto', component: ContactoComponent},
   {path: 'index', component:  IndexComponent },
+  {path:'equipo', component: EquiposComponent},
   { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
 
