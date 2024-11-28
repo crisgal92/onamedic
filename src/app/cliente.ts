f0e5cee4414bb134cdf25d2e79aed32370485a93
crisgal92 <cristian_agm92@hotmail.com>
2024-11-27 21:05:25 -0600

    Se crea un componente llamado cliente-list es el dashboard para  ver lo que tenemos almacenado en la base de datos ahi mismo se crea el metodo de eliminar el registro, en el formulario de contacto se agrega el metodo de post que sirve para agregar registros en la base de datos, se agrega en el nav bar un apartado que dice listado es solo para verificar que funcionaban los metodos agregados.

---
+++ src/app/cliente.ts
@@ -0,0 +1,6 @@
+export class Cliente {
+    constructor(public id:number, 
+        public nombre : string , 
+        public correo : string, 
+        public descripcion : string){}
+}
--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ClienteService } from '../service/cliente.service';
+import { Cliente } from '../cliente';
 
 @Component({
   selector: 'app-formulario',
   templateUrl: './formulario.component.html',
   styleUrl: './formulario.component.css'
 })
-export class FormularioComponent {
+export class FormularioComponent implements OnInit{
+  id: number=0;
+nombre: string ='';
+correo : string='';
+descripcion: string ='';
+constructor (private clienteService : ClienteService){}
+
+  ngOnInit(): void {   
+  }
+  addCliente(){
+    let cliente = new Cliente(this.id, this.nombre, this.correo, this.descripcion);
+    console.log(cliente);
+    this.clienteService.createCliente(cliente).subscribe(
+      res=> console.log(res)
+    );
+  }
 
 }
